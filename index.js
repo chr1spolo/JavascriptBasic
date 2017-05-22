14a07ef74f2fcadded96ca76a4d124620efae0b5
Christian Polo <polidavis28@gmail.com>
2017-05-21 21:37:43 -0500

    arrow functions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let height = 7;
 // o simplemente quitar las llaves y si es una sola 
 // ejecución, no hace falta ni colocar return
 
-let triangleArea = (base, height) => base * height / 2;
+const triangleArea = (base, height) => base * height / 2;
 
 console.log(`El área de un triángulo de base
 ${base} y alltura ${height} es : ${triangleArea(base, height)}`);
