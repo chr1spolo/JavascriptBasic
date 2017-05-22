11ab06e84d521b3ca9a2658cfa87771691a2c567
Christian Polo <polidavis28@gmail.com>
2017-05-21 21:33:14 -0500

    Arrow functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(`El area de un triangulo de base 5 y altura 7 es: ${5 * 7 / 2}`);
+let base = 5;
+let height = 7;
+
+
+//arrows functions javascript
+// puede llevar llaves en caso de tener muchas lineas, 
+// o simplemente quitar las llaves y si es una sola 
+// ejecución, no hace falta ni colocar return
+
+let triangleArea = (base, height) => base * height / 2;
+
+console.log(`El área de un triángulo de base
+${base} y alltura ${height} es : ${triangleArea(base, height)}`);
